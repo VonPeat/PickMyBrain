fd770505d128788c4e3a16534a72ea5077aa3763
Peter <peter@yellowtwig.nl>
2024-03-22 23:43:03 +0100

    First commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,12 @@
+import '../styles/styles.css'
+
+
+// React Related code goes here
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+if (module.hot) {
+    module.hot.accept()
+}
